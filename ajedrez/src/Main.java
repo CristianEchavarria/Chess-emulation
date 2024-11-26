679bd8cf392883f62cf45a4bc9582b8c32a693a7
CristianEchavarria <cristianechavarriacastro@gmail.com>
2024-11-25 22:48:58 -0500

    Delete ajedrez directory

--- ajedrez/src/Main.java
+++
@@ -1,8 +0,0 @@
-import controller.ChessController;
-
-public class Main {
-    public static void main(String[] args) {
-        ChessController controller = new ChessController();
-        controller.startGame();
-    }
-}
